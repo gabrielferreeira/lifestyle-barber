9bfd5bb79f6d94c90f76ef9b65296de457888e6c
gabriel lopes <contatogabriieldec@gmail.com>
2024-02-24 11:32:26 -0300

    feito a parte mobile e adicionado as animações com uma biblioteca externa chamada ScrollReveal

---
+++ assets/javascript/efeito.js
@@ -0,0 +1,40 @@
+window.efeito = ScrollReveal({ reset: true });
+
+efeito.reveal(".efeito1", {
+  duration: 2000,
+  distance: "50px",
+  delay: 500,
+});
+
+efeito.reveal(".efeito2", {
+  duration: 2000,
+  distance: "50px",
+  delay: 1000,
+});
+
+efeito.reveal(".efeito3", {
+  duration: 2000,
+  distance: "50px",
+  delay: 1500,
+});
+
+efeito.reveal(".efeito-left", {
+  duration: 2000,
+  distance: "60px",
+  delay: 500,
+  origin: "left",
+});
+
+efeito.reveal(".efeito-right", {
+  duration: 2000,
+  distance: "60px",
+  delay: 1000,
+  origin: "right",
+});
+
+efeito.reveal(".efeito-banner", {
+  duration: 2000,
+  distance: "40px",
+  delay: 500,
+  origin: "top",
+});
